72d7e2d007004841ab7620746426ea0d3e7e7627
sherryLi <sherryli@sherryLideMacBook-Pro.local>
2020-05-27 01:23:49 -0500

    database update

---
+++ Vicabulary/app/src/main/java/com/laioffer/vicabulary/DatabaseHelper.java
@@ -0,0 +1,51 @@
+package com.laioffer.vicabulary;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Video.db";
+    public static final String TABLE_NAME = "video_table";
+    public static final String VIDEO_ID = "ID";
+    public static final String VIDEO_NAME = "NAME";
+    public static final String VIDEO_URL = "URL";
+    public static final String VIDEO_SUBTITLE = "SUBTITLE";
+    public static final String VIDEO_COVER = "COVER";
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, URL TEXT, SUBTITLE TEXT, COVER TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name, String url, String subtitle, String cover) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(VIDEO_NAME, name);
+        contentValues.put(VIDEO_URL, url);
+        contentValues.put(VIDEO_SUBTITLE, subtitle);
+        contentValues.put(VIDEO_COVER, cover);
+        long result =  db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
